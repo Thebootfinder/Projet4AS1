cda1750f7d39339969020be85f3ade06a298cb89
Hugo Bleuze <bleuze@et.esiea.fr>
2019-10-19 20:31:23 +0200

    Register+CreerUser

--- server.js
+++ server.js
@@ -50,9 +50,29 @@ app.post('/api/login', (req, res) => {
         status: true
       })
     }
-  } else {
-    res.status(401)
   }
+})
+
+app.post('/api/Register', (req, res) => {
+  const test = users.find(u => u.ID === req.body.username)
+  if (!test) {
+    res.json({
+      status: true
+    })
+  } else {
+    res.json({
+      status: false
+    })
+  }
+})
+
+app.post('/api/CreerUser', (req, res) => {
+  console.log('req.body', req.body)
+  console.log('req.query', req.query)
+  users.push({
+    ID: req.body.username,
+    Pass: req.body.password
+  })
 })
 
 const port = process.env.PORT || 4000
